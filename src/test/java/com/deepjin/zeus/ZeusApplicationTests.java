0fb0f8bed5cd4e843e91bb44847e22e573dd4aeb
C-YooJin <ioi_3312@naver.com>
2021-07-21 15:01:58 +0900

    Initial

---
+++ src/test/java/com/deepjin/zeus/ZeusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepjin.zeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
